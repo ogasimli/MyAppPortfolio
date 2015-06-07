24765f388d29eb16b814018ffb5565a582f698dc
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-06-08 00:51:00 +0500

    Fix typo in toast message text

--- app/src/main/java/org/ogasimli/myappportfolio/MainActivity.java
+++ app/src/main/java/org/ogasimli/myappportfolio/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) view;
         String buttonText = (String) button.getText();
         Context context = getApplicationContext();
-        CharSequence text = getString(R.string.openApp)+buttonText;
+        CharSequence text = getString(R.string.openApp)+buttonText+"!";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.setGravity(Gravity.BOTTOM| Gravity.CENTER, 0, 10);
