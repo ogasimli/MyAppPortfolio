b8b7e71e74df0c6427cd88ea1f3dfb8da11a1240
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2015-06-07 23:25:26 +0500

    Resolve bug with toast messages

--- app/src/main/java/org/ogasimli/myappportfolio/MainActivity.java
+++ app/src/main/java/org/ogasimli/myappportfolio/MainActivity.java
@@ -2,6 +2,7 @@ package org.ogasimli.myappportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,23 +12,48 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-/*    final Button buttonAppOne = (Button) findViewById(R.id.buttonAppOne);
-    final Button buttonAppTwo = (Button) findViewById(R.id.buttonAppTwo);
-    final Button buttonAppThree = (Button) findViewById(R.id.buttonAppThree);
-    final Button buttonAppFour = (Button) findViewById(R.id.buttonAppFour);
-    final Button buttonAppFive = (Button) findViewById(R.id.buttonAppFive);
-    final Button buttonAppSix = (Button) findViewById(R.id.buttonAppSix);*/
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onItemClick(View view) {
-        Toast.makeText(getApplicationContext(),
-                "This button will launch "+ view.getContext()+"!", Toast.LENGTH_LONG).show();
+    public void onClick(View v) {
+        Button buttonAppOne = (Button) findViewById(R.id.buttonAppOne);
+        Button buttonAppTwo = (Button) findViewById(R.id.buttonAppTwo);
+        Button buttonAppThree = (Button) findViewById(R.id.buttonAppThree);
+        Button buttonAppFour = (Button) findViewById(R.id.buttonAppFour);
+        Button buttonAppFive = (Button) findViewById(R.id.buttonAppFive);
+        Button buttonAppSix = (Button) findViewById(R.id.buttonAppSix);
+        CharSequence text = null;
+        switch (v.getId()) {
+            case R.id.buttonAppOne:
+                text=buttonAppOne.getText();
+                break;
+            case R.id.buttonAppTwo:
+                text=buttonAppTwo.getText();
+                break;
+            case R.id.buttonAppThree:
+                text=buttonAppThree.getText();
+                break;
+            case R.id.buttonAppFour:
+                text=buttonAppFour.getText();
+                break;
+            case R.id.buttonAppFive:
+                text=buttonAppFive.getText();
+                break;
+            case R.id.buttonAppSix:
+                text=buttonAppSix.getText();
+                break;
+            default:
+                break;
+        }
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "This button will launch "+ text +"!", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM| Gravity.CENTER, 0, 10);
+        toast.show();
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
